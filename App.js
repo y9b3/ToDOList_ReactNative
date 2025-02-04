1d95c768f081762d587f6059a0fc15f48b79f24a
Yanis <yanis.boudjelal@outlook.fr>
2025-02-04 15:52:28 +0100

    Second commit : Création du Layout des séction de l'app

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { s } from "./App.style";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>Hello !!</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={s.app}>
+          <View style={s.header}>
+            <Text>Header</Text>
+          </View>
+          <View style={s.body}>
+            <Text>body</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={s.footer}>
+        <Text>footer</Text>
+      </View>
+    </>
   );
 }
---
+++ App.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const s = StyleSheet.create({
+  app: {
+    backgroundColor: "#F9F9F9",
+    flex: 1,
+  },
+  header: { flex: 1 },
+
+  body: { flex: 5 },
+
+  footer: { height: 70 },
+});
+
+export { s };

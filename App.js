bb4bd400893fffecbf2e712df80eabc294a3ca8b
Yanis <yanis.boudjelal@outlook.fr>
2025-02-04 18:40:42 +0100

    Ajout de la liste des tache_Exemple

--- App.js
+++ App.js
@@ -2,6 +2,18 @@ import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
 import { Header } from "./components/Header/Header";
+import { CardToDo } from "./components/CardToDo/CardTodo";
+
+const TODO_LIST = [
+  { id: 1, title: "Jouer à R6", isCompleted: true },
+  { id: 2, title: "Aller chez le garagiste", isCompleted: false },
+  { id: 3, title: "Faire les courses", isCompleted: true },
+  { id: 4, title: "Appeler le vétérinaire", isCompleted: true },
+  { id: 5, title: "Sortir le chien", isCompleted: true },
+  { id: 6, title: "Aller chez le garagiste", isCompleted: false },
+  { id: 7, title: "Faire les courses", isCompleted: true },
+  { id: 8, title: "Appeler le vétérinaire", isCompleted: true },
+];
 
 export default function App() {
   return (
@@ -12,7 +24,7 @@ export default function App() {
             <Header></Header>
           </View>
           <View style={s.body}>
-            <Text>body</Text>
+            <CardToDo todo={TODO_LIST[0]}></CardToDo>
           </View>
         </SafeAreaView>
       </SafeAreaProvider>
